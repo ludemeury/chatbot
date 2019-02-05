6299d715421808b605fe19f5e1cc36d990241919
ludemeury <ludemeury@gmail.com>
2019-02-05 06:51:08 -0200

    production database conf

--- config/database.rb
+++ config/database.rb
@@ -1,21 +1,31 @@
 configure :test do
-    set :database, {
+  set :database,
       adapter: 'postgresql',
       encoding: 'utf8',
       database: 'chatbot_test',
       pool: 5,
       username: 'postgres',
       host: 'postgres'
-    }
-  end
-     
-  configure :development do
-    set :database, {
+end
+
+configure :development do
+  set :database,
       adapter: 'postgresql',
       encoding: 'utf8',
       database: 'chatbot_development',
       pool: 5,
       username: 'postgres',
       host: 'postgres'
-    }
-  end
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/chatbot_production')
+
+  set :database,
+      adapter: 'postgresql',
+      host: db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+end
